ad2ee49cc5fd84cbb835a7ec904dadf2966cd965
Stuart MacCallum <maccalsa@googlemail.com>
2025-05-04 14:31:51 +0100

    Add and remove scripts

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/maccalsa/bashhub
 go 1.23.0
 
 require (
+	github.com/gdamore/tcell/v2 v2.8.1
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.28
 	github.com/rivo/tview v0.0.0-20250501113434-0c592cd31026
@@ -10,7 +11,6 @@ require (
 
 require (
 	github.com/gdamore/encoding v1.0.1 // indirect
-	github.com/gdamore/tcell/v2 v2.8.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
--- internal/tui/ui.go
+++ internal/tui/ui.go
@@ -2,7 +2,7 @@ package tui
 
 import (
 	"fmt"
-
+	"github.com/gdamore/tcell/v2"
 	"github.com/jmoiron/sqlx"
 	"github.com/rivo/tview"
 	"github.com/maccalsa/bashhub/internal/database"
@@ -13,6 +13,7 @@ type UI struct {
 	db      *sqlx.DB
 	list    *tview.List
 	details *tview.TextView
+	root    tview.Primitive // root primitive
 }
 
 func NewUI(db *sqlx.DB) *UI {
@@ -55,13 +56,88 @@ func (ui *UI) loadScripts() {
 	}
 }
 
+func (ui *UI) confirmDeleteScript() {
+	index := ui.list.GetCurrentItem()
+	if index < 0 {
+		return
+	}
+
+	name, _ := ui.list.GetItemText(index)
+
+	modal := tview.NewModal().
+		SetText(fmt.Sprintf("Delete script '%s'?", name)).
+		AddButtons([]string{"Cancel", "Delete"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonLabel == "Delete" {
+				scripts, _ := database.GetScripts(ui.db)
+				scriptID := scripts[index].ID
+				if err := database.DeleteScript(ui.db, scriptID); err != nil {
+					ui.details.SetText(fmt.Sprintf("[red]Failed to delete script: %v", err))
+				} else {
+					ui.loadScripts()
+					ui.details.SetText("[green]Script deleted.")
+				}
+			}
+			ui.app.SetRoot(ui.root, true)
+		})
+
+	ui.app.SetRoot(modal, false)
+}
+
+
+func (ui *UI) showCreateForm() {
+	form := tview.NewForm()
+
+	form.AddInputField("Name", "", 20, nil, nil).
+		AddInputField("Description", "", 40, nil, nil).
+		AddTextArea("Content", "", 60, 10, 0, nil).
+		AddButton("Save", func() {
+			name := form.GetFormItemByLabel("Name").(*tview.InputField).GetText()
+			description := form.GetFormItemByLabel("Description").(*tview.InputField).GetText()
+			content := form.GetFormItemByLabel("Content").(*tview.TextArea).GetText()
+
+			script := database.Script{Name: name, Description: description, Content: content}
+			if err := database.CreateScript(ui.db, script); err != nil {
+				ui.details.SetText(fmt.Sprintf("[red]Failed to create script: %v", err))
+			} else {
+				ui.loadScripts()
+				ui.details.SetText("[green]Script created successfully.")
+			}
+			ui.app.SetRoot(ui.root, true)
+		}).
+		AddButton("Cancel", func() {
+			ui.app.SetRoot(ui.root, true)
+		})
+
+	form.SetBorder(true).SetTitle("New Script").SetTitleAlign(tview.AlignLeft)
+	ui.app.SetRoot(form, true)
+}
+
+func (ui *UI) handleKeys(event *tcell.EventKey) *tcell.EventKey {
+	switch event.Rune() {
+	case 'C', 'c':
+		ui.showCreateForm()
+	case 'D', 'd':
+		ui.confirmDeleteScript()
+	}
+	return event
+}
+
 func (ui *UI) Run() error {
 	ui.loadScripts()
 
-	flex := tview.NewFlex().
+	ui.list.SetBorder(true).SetTitle("Scripts (Press [green]C[white]=Create, [red]D[white]=Delete)")
+	ui.details.SetBorder(true).SetTitle("Details")
+
+	ui.list.SetDoneFunc(func() {
+		ui.app.Stop()
+	})
+
+	ui.list.SetInputCapture(ui.handleKeys)
+
+	ui.root = tview.NewFlex().
 		AddItem(ui.list, 0, 1, true).
 		AddItem(ui.details, 0, 2, false)
 
-	ui.app.SetRoot(flex, true).SetFocus(ui.list)
-	return ui.app.Run()
+	return ui.app.SetRoot(ui.root, true).Run()
 }
