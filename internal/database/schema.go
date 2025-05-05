bc2a0658d5c4775603e6de713027509363b820e4
Stuart MacCallum <maccalsa@googlemail.com>
2025-05-05 10:27:06 +0100

    set db location

--- internal/database/schema.go
+++ internal/database/schema.go
@@ -2,6 +2,9 @@ package database
 
 import (
 	"log"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
@@ -15,8 +18,28 @@ CREATE TABLE IF NOT EXISTS scripts (
 	content TEXT NOT NULL
 );`
 
+func getDBPath() string {
+    var baseDir string
+
+    switch runtime.GOOS {
+    case "darwin":
+        baseDir = filepath.Join(os.Getenv("HOME"), "Library", "Application Support", "bashhub")
+    case "linux":
+        baseDir = filepath.Join(os.Getenv("HOME"), ".config", "bashhub")
+    default:
+        log.Fatal("Unsupported OS")
+    }
+
+    if err := os.MkdirAll(baseDir, os.ModePerm); err != nil {
+        log.Fatalf("Failed to create config directory: %v", err)
+    }
+
+    return filepath.Join(baseDir, "bashhub.db")
+}
+
+
 func ConnectDB() *sqlx.DB {
-	db, err := sqlx.Connect("sqlite3", "scripts.db")
+	db, err := sqlx.Connect("sqlite3", getDBPath())
 	if err != nil {
 		log.Fatalln(err)
 	}
