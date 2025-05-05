c0f6bea90c6f5c8f6c4d741f62b617b8e61ab5c5
Stuart MacCallum <maccalsa@googlemail.com>
2025-05-05 22:51:09 +0100

    Fix laceholder form

--- internal/tui/placeholders.go
+++ internal/tui/placeholders.go
@@ -1,6 +1,7 @@
 package tui
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/maccalsa/bashhub/internal/database"
 	"github.com/maccalsa/bashhub/internal/executor"
 	"github.com/rivo/tview"
@@ -10,6 +11,7 @@ import (
 func (ui *UI) promptPlaceholderInputs(script database.Script, placeholders []string) {
 	inputs := make(map[string]string)
 	form := tview.NewForm()
+	ui.inForm = true
 
 	for _, ph := range placeholders {
 		form.AddInputField(ph, "", 30, nil, func(text string) {
@@ -19,11 +21,20 @@ func (ui *UI) promptPlaceholderInputs(script database.Script, placeholders []str
 
 	form.AddButton("Run", func() {
 		finalScript := executor.ReplacePlaceholders(script.Content, inputs)
+		ui.inForm = false
 		ui.runAndDisplay(finalScript)
 	}).AddButton("Cancel", func() {
+		ui.inForm = false
 		ui.app.SetRoot(ui.root, true)
 	})
 
 	form.SetBorder(true).SetTitle("Fill placeholders").SetTitleAlign(tview.AlignLeft)
+	form.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEscape {
+			ui.inForm = false
+			ui.app.SetRoot(ui.root, true)
+		}
+		return event
+	})
 	ui.app.SetRoot(form, true).SetFocus(form)
 }
